8778401d52f3c2bbf42a9c1f9fd56d6a8c1867e8
yaniky <295958179@qq.com>
2019-07-16 10:37:10 +0800

    version(0.0.1): 初版，仅支持pc

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,23 +1,23 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.base.js');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require("webpack-merge");
+const common = require("./webpack.base.js");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: 'inline-source-map',
-  devServer: { 
-    contentBase: '../dist'
-  },
-  entry: './src/index-dev.js',
-  output: { 
-    filename: 'js/[name].[hash].js', 
-    path: path.resolve(__dirname, '../dist')
-  },
-  module: {},
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'),
-    })
-  ]
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "../dist"
+    },
+    entry: "./src/index-dev.js",
+    output: {
+        filename: "js/[name].[hash].js",
+        path: path.resolve(__dirname, "../dist")
+    },
+    module: {},
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../public/index.html")
+        })
+    ]
 });
